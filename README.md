2a33b907f991f2103aef6a258a5e838e592fd1cf
stphngrtz <stephan.goertz@gmail.com>
2018-02-08 09:30:58 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Issue 144
+
+[Issue #144: Spring Security integration, for user authentication and authorization in gateway](https://github.com/spring-cloud/spring-cloud-gateway/issues/144)
+
+```bash
+mvn clean package
+java -jar target\issue144-1.0-SNAPSHOT.jar
+curl -u user:pw http://localhost:8080/get
+```
---
+++ src/main/java/com/example/issue144/Issue144Application.java
@@ -0,0 +1,57 @@
+package com.example.issue144;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@SpringBootApplication
+@EnableWebFluxSecurity
+public class Issue144Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Issue144Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Issue144Application.class, args);
+    }
+
+    @Bean
+    MapReactiveUserDetailsService authentication() {
+        return new MapReactiveUserDetailsService(
+                User.withDefaultPasswordEncoder().username("user").password("pw").roles("USER").build()
+        );
+    }
+
+    @Bean
+    SecurityWebFilterChain authorization(ServerHttpSecurity security) {
+        return security.authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .build();
+    }
+
+    @Bean
+    RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("httpbin", r -> r
+                        .order(8000)
+                        .path("/**")
+                        .filter((exchange, chain) -> {
+                            exchange.getPrincipal().doOnNext(principal -> log.info("principal.name: {}", principal.getName()));
+                            return chain.filter(exchange);
+                        })
+                        .uri("http://httpbin.org:80")
+                )
+                .build();
+    }
+}
